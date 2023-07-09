a43afe3b8e9c622a6e55ea0b7f6c02815995b25d
St2fy <zhihao2015jl@gmail.com>
2023-07-09 02:33:17 -0700

    Health bar changes

--- Player/interface.gd
+++ Player/interface.gd
@@ -3,9 +3,14 @@ extends Control
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$HeroBar/FullHealth.visible = false
 	$CoinCounter/Label.text = str(Global.Coins)
 	if Input.is_action_just_pressed("Swap"):
 		$Mode/Obstacle.visible = !$Mode/Obstacle.visible
 		$Mode/Minion.visible = !$Mode/Minion.visible
+	if $HeroBar/Health.value == $HeroBar/Health.max_value:
+		$HeroBar/FullHealth.visible = true
+		
+	
 	
 	
