89abf20c4dfa7f297436bf5e5de05de37d1052b0
St2fy <zhihao2015jl@gmail.com>
2023-07-09 01:29:27 -0700

    ui

--- Player/interface.gd
+++ Player/interface.gd
@@ -1,12 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Mode/Obstacle.visible = false
-	$Mode/Minion.visible = true
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$CoinCounter/Label.text = str(Global.Coins)
